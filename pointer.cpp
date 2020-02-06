d7ec0b11b8cb02c70374f3bca6ede3520b832335
vietcuong104 <vietcuong104@gmail.com>
2020-02-06 10:39:44 +0700

    add pointer.cpp file

---
+++ pointer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // int *p;
+    // int value = 5;
+    // p = &value;
+
+    // cout<<"&value is: "<<&value<<endl;
+    // cout<<"value of p is: "<<p<<endl;
+    // cout<<"&p is: "<<*p;
+
+    // *p = 100;
+    // cout<<"\nvalue changed is: "<<value;
+
+    int arr[] = {1,2,3,4};
+    int *q = &arr[0];
+    cout<<"value of arr[] is: "<<arr<<endl;
+    cout<<"value of q is: "<<q<<endl;
+    cout<<"&q is: "<<&q;
+
+    q++;
+    cout<<"value of q is: "<<q<<endl;
+    cout<<"&q is: "<<*q<<endl;
+
+    q+=2;
+    cout<<"value of q is: "<<q<<endl;
+    cout<<"&q is: "<<*q<<endl;
+   
+    q--;
+    cout<<"value of q is: "<<q<<endl;
+    cout<<"&q is: "<<*q<<endl;
+
+    (*q) +=3 ;
+    cout<<"value of q is: "<<q<<endl;
+    cout<<"&q is: "<<*q<<endl;   
+
+}
